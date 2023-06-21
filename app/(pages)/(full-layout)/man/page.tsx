785afd520302ced476ee0e35808bd6129e0ccceb
lyk <lyk@ibksystem.co.kr>
2023-06-21 18:26:40 +0900

    title 수정

--- app/(pages)/(full-layout)/man/page.tsx
+++ app/(pages)/(full-layout)/man/page.tsx
@@ -24,7 +24,7 @@ const Main:React.FC = () => {
      */
     React.useEffect(() => {
         const htmlTitle = document.querySelector('title')
-        htmlTitle.innerHTML = '클라우드 포탈 관리자'
+        htmlTitle.innerHTML = 'Next Starter Kit'
         
         setDataTable()
     }, []);
